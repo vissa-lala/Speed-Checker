b8cba29855ebdfc4bcc61916cffea3b909b36b5b
vis hnu <vishnu.sl1818@gmail.com>
2026-05-30 19:17:27 +0530

    code fix 2

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✓ Server running on port ${PORT}`);
 });
